be8049955137becf71b6adcecbce89e8fde0eba2
OBaca <78699182+OBaca@users.noreply.github.com>
2022-09-30 12:47:47 +0300

    Update
    reorganize the code.

--- jetpack/main.py
+++ jetpack/main.py
@@ -1,245 +1,85 @@
-from data.constants import *
-from data.player import *
-from data.obstacles import *
-from data.map import *
-from data.lasers import *
-from data.button import *
-from data.missile import *
-from data.coin import *
-from data.coins import *
 from data.zombie import *
-
-pygame.init()
+from data.game import *
+from data.costumes import *
 
 
 # screen settings
 SCREEN = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption('Jetpack Joyride')
+pygame.display.set_icon(JETPACK_OFF_MALE)
 
-def update_high_score(high_score, score):
-    if high_score < score:
-        high_score = score
-    with open('high_score.txt','w') as file:
-        file.write(str(high_score))
-    return high_score
-
-def get_high_score():
-    try:
-        high_score = int(read_high_score())
-    except:
-        high_score = 0
-    return high_score
-
-def read_high_score():
-    with open("high_score.txt", 'r') as file:
-        return file.read()
-
-def show_high_score(screen, high_score):
-    high_score_text = SCORE_FONT.render('HIGH SCORE: ' + str(high_score), 1, BLACK)
-    screen.blit(high_score_text, (10,50))
-
-def show_score(screen, score):
-    score_text = SCORE_FONT.render('SCORE: ' + str(score), 1, BLACK)
-    screen.blit(score_text, (10,10))
-
-
-def main():
-    map = Map()
-    # to make a shrek avatar, 150X200 // 60X75
-    player = Player() 
-    obstacles = []
-    obstacles.append(Zappers(pygame.Rect(random.randint(WIDTH, WIDTH+200),random.randint(FIX_IMAGE_LIMIT, HEIGHT-140),40,140) ,  'along', OBS_IMG1))  #300,500,40,140,
-    obstacles.append(Zappers(pygame.Rect(random.randint(WIDTH, WIDTH+200),random.randint(FIX_IMAGE_LIMIT, HEIGHT- FIX_IMAGE_LIMIT -40),140,40) , 'across', OBS_IMG2))  #300,450,140, 40,
-    
-    lasers = [
-                Lasers(pygame.Rect(0, HIDDEN_LASERS_Y, 900, 50), pygame.Rect(0, HIDDEN_LASERS_Y, 900, 50) ),
-                Lasers(pygame.Rect(0, HIDDEN_LASERS_Y, 900, 50), pygame.Rect(0, HIDDEN_LASERS_Y, 900, 50) ),
-                Lasers(pygame.Rect(0, HIDDEN_LASERS_Y, 900, 50), pygame.Rect(0, HIDDEN_LASERS_Y, 900, 50) ),
-                Lasers(pygame.Rect(0, HIDDEN_LASERS_Y, 900, 50), pygame.Rect(0, HIDDEN_LASERS_Y, 900, 50) )
-            ]
-    missiles = [
-                Missile(pygame.Rect(WIDTH+10,player.rect.y, 100, 100)),
-                Missile(pygame.Rect(WIDTH+10,player.rect.y, 100, 100))
-                ]
-    
-    coins = Coins()
-
-    resume_button = Button(pygame.Rect(WIDTH//2 - 75, HEIGHT//2 - 120, 150, 100), RESUME_NO_HOVER_BUTTON)
-    retry_button = Button(pygame.Rect(WIDTH//2 - 75, HEIGHT//2, 150, 100), RETRY_BUTTON)
-    quit_button = Button(pygame.Rect(WIDTH//2 - 75, HEIGHT//2 + 120, 150, 100), QUIT_GAME_BUTTON)
-    speaker_off_button = Button(pygame.Rect(WIDTH//2 - 75, HEIGHT//2 - 200, 100, 50), SPEAKER_OFF_BUTTON)
-    speaker_on_button = Button(pygame.Rect(WIDTH//2 + 15, HEIGHT//2 - 200, 100, 50), SPEAKER_ON_BUTTON)
-
-
-    score_speed = 1
-    score = 0
-    high_score = get_high_score()
-    pause_game = False
-    silent_music = False
-    
-    
-    
-    active = True   # to check if the player can still play
-    run = True
-    clock = pygame.time.Clock()
-    while run:
-        map.redraw_map(SCREEN)
-        if pause_game:
-            
-            # resume button
-            if resume_button.draw_button(SCREEN, 'resume'):
-                pause_game = False
-
-            # retry button
-            if retry_button.draw_button(SCREEN, 'retry'):
-                # 1. reset score
-                score = 0
-                score_speed = 1
-                reset_lasers(lasers, score)
-                # 2. reset map location
-                map.reset()
-                # 3. reset player location
-                player.reset()
-                # 4. reset obstacle positions
-                reset_obstacle_pos(obstacles, lasers)
-                # 5. reset coins system
-                coins.reset()
-                
-                # need to fix reset when you pause the game and try to press on the retry button while lasers ON
-                continue
-    
-            # quit button
-            if quit_button.draw_button(SCREEN, 'quit_game'):
-                run = False
-            # turn off music and sfx
-            if speaker_off_button.draw_button(SCREEN, 'speaker-off'):
-                pygame.mixer.pause()
-                silent_music = True
-            # turn on music and sfx
-            if speaker_on_button.draw_button(SCREEN, 'speaker-on'):
-                pygame.mixer.unpause()
-                silent_music = False
-
-        else:
-            
-            map.update()
-                        
-            pressed_key = pygame.key.get_pressed()
-            player.player_movement(pressed_key)
-
-            # adding score
-            score_speed += 0.1
-            score = int(score_speed)
-            
-            
-            player.draw(SCREEN, map)
-
-            # manage coins system
-            coins.draw(SCREEN)
-            coins.update()
-            coin_collect(player, coins)
-    
-
-            draw_obstacles(obstacles,lasers,missiles, SCREEN)
-
-            # place lasers algorithm
-            lasers_placement(score,lasers,silent_music)
-
-            # place missiles algorithem
-            missile_movement(missiles, score, player, silent_music)
-
-            
-            obstacle_placement(obstacles)
-            check_hit_obstacles(player, obstacles)
-
-            
-            # show score
-            show_score(SCREEN, score)
-            # show high score
-            high_score = update_high_score(high_score, score)
-            show_high_score(SCREEN, high_score)
-
-            increase_speed(map)
-
-            clock.tick(FPS)
-
-        for event in pygame.event.get():
-
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-                
-
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    pause_game = True
-            
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_SPACE:
-                    player.image = JETPACK_OFF
-                
-
-            if event.type == PLAYER_HIT:
-                print('game over')
-                # need to add animation of the player falling over
-                # showing high score, and current score, and updating the high score
-                    
-
-        pygame.display.update()
-
-
-    pygame.quit()
-
-
-
-
+''' This function manage the main menu screen. '''
 def main_menu():
     clock = pygame.time.Clock()
-
+    click = False   # to know if a user pressed a button.
+    # buttons
     play_button = Button(pygame.Rect(100,550,MENU_BUTTON_WIDTH,MENU_BUTTON_HEIGHT),  PLAY_MENU_NO_HOVER_BUTTON)
     costumes_button = Button(pygame.Rect(350,550,MENU_BUTTON_WIDTH,MENU_BUTTON_HEIGHT),  COSTUMES_MENU_NO_HOVER_BUTTON)
     quit_button = Button(pygame.Rect(600,550,MENU_BUTTON_WIDTH,MENU_BUTTON_HEIGHT),  QUIT_MENU_NO_HOVER_BUTTON)
     
+    # initialize the player and zombies.
+    player = Player(JETPACK_OFF_MALE, 0) 
     zombies = [Zombie(True, -120), Zombie(False, WIDTH), Zombie(True, -130), Zombie(False, WIDTH+10)]
     
-    
+    # music settings.
+    pygame.mixer.music.set_volume(0.4)
     music_channel = pygame.mixer.find_channel()
-    music_channel.play(MENU_MUSIC, -1) 
+    music_replaying = False
     
+    # main menu loop.
     while True:
+        coins_amount = get_coins()
+
+        # start of the main menu music.
+        if music_replaying == False:
+            music_channel.play(MENU_MUSIC, -1)
+            music_replaying = True
         
-        #MENU_MUSIC.play()
+        # set background
         SCREEN.fill((255,255,255))
         SCREEN.blit(MENU_BG_IMG, (0,0))
-        if play_button.draw_button(SCREEN, 'play'):
-            music_channel.pause()
-            main()
 
-        if quit_button.draw_button(SCREEN, 'quit_menu'):
+        # when you press the play button you will start the game.
+        if play_button.draw_button(SCREEN, 'play'):
+            if click:
+                music_channel.pause()
+                game(coins_amount, player, SCREEN)
+                music_replaying = False
+
+        # when you press the quit button you will exit the game.
+        elif quit_button.draw_button(SCREEN, 'quit_menu'):
             pygame.quit()
             sys.exit()
         
-        if costumes_button.draw_button(SCREEN, 'costumes'):
-            pass
+        # when you press the costumes button you will move to the costumes screen.
+        elif costumes_button.draw_button(SCREEN, 'costumes'):
+            if click:
+                costumes_menu(player, SCREEN)
+
+        click = False
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
         
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True
+
+        # spawn zombies to the main menu.
         zombie_spawn_menu(zombies, SCREEN)
+
+        # show coins on the screen.
+        show_menu_coins(SCREEN,coins_amount)
 
         clock.tick(FPS)
         pygame.display.update()
 
-    
 
+# program start.
+def main():
+    main_menu()
 
-main_menu()
-'''
 if __name__ == "__main__":
     main()
-'''
-
-
-
