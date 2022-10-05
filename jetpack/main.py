d00b1d9f497ce89e9287177f6978df6a27c05c9e
OBaca <78699182+OBaca@users.noreply.github.com>
2022-10-05 12:19:45 +0300

    update

--- jetpack/main.py
+++ jetpack/main.py
@@ -2,7 +2,7 @@ from data.zombie import *
 from data.game import *
 from data.costumes import *
 from data.button import *
-from os.path import abspath
+
 
 # screen settings
 SCREEN = pygame.display.set_mode((WIDTH,HEIGHT))
@@ -14,9 +14,9 @@ def main_menu():
     clock = pygame.time.Clock()
     click = False   # to know if a user pressed a button.
     # buttons
-    play_button = Button(pygame.Rect(100,550,MENU_BUTTON_WIDTH,MENU_BUTTON_HEIGHT),  PLAY_MENU_NO_HOVER_BUTTON)
-    costumes_button = Button(pygame.Rect(350,550,MENU_BUTTON_WIDTH,MENU_BUTTON_HEIGHT),  COSTUMES_MENU_NO_HOVER_BUTTON)
-    quit_button = Button(pygame.Rect(600,550,MENU_BUTTON_WIDTH,MENU_BUTTON_HEIGHT),  QUIT_MENU_NO_HOVER_BUTTON)
+    play_button = Button(pygame.Rect(100,550,MENU_BUTTON_WIDTH,MENU_BUTTON_HEIGHT),  PLAY_MENU_NO_HOVER_BUTTON, PLAY_MENU_HOVER_BUTTON)
+    costumes_button = Button(pygame.Rect(350,550,MENU_BUTTON_WIDTH,MENU_BUTTON_HEIGHT),  COSTUMES_MENU_NO_HOVER_BUTTON, COSTUMES_MENU_HOVER_BUTTON)
+    quit_button = Button(pygame.Rect(600,550,MENU_BUTTON_WIDTH,MENU_BUTTON_HEIGHT),  QUIT_MENU_NO_HOVER_BUTTON, QUIT_MENU_HOVER_BUTTON)
     
     # initialize the player and zombies.
     player = Player(JETPACK_OFF_MALE, 0) 
@@ -26,6 +26,7 @@ def main_menu():
     pygame.mixer.music.set_volume(0.4)
     music_channel = pygame.mixer.find_channel()
     music_replaying = False
+    
     
 
     # main menu loop.
